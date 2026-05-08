039b81459144317912b1fc6f9842cb4a9dded0a9
tharanyaa <66083038+tharanyaa@users.noreply.github.com>
2026-05-08 01:07:16 +0100

    Delete rotz2d.m

--- rotz2d.m
+++
@@ -1,9 +0,0 @@
-function v_rot = rotz2d(v, ang)
-% Rotate a 3x1 vector in the xy-plane by angle ang about +z.
-
-R = [cos(ang) -sin(ang) 0;
-     sin(ang)  cos(ang) 0;
-     0         0        1];
-
-v_rot = R * v;
-end
